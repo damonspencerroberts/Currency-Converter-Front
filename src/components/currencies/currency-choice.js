5d5e532e073ec737e29f826665288584981a9fec
Damon Roberts <damonspencerroberts@gmail.com>
2021-03-17 20:38:50 +0000

    Add currencies

--- src/components/currencies/currency-choice.js
+++ src/components/currencies/currency-choice.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const currencyChoice = () => {
+const CurrencyChoice = (props) => {
+  const [current, setCurrent] = useState({});
+
+  console.log(current);
   return (
     <div>
-    <label for="currency">Choose a currency:</label>
+      <label for="currency">Choose a base currency:</label>
 
-    <select name="currency" id="currency">
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="mercedes">Mercedes</option>
-      <option value="audi">Audi</option>
-    </select>
+      <select name="currency" id="currency" onChange = {(e) => setCurrent(e.target.value)}>
+        <option value={props.gbp.base}>{props.gbp.base}</option>
+        <option value={props.chf.base}>{props.chf.base}</option>
+        <option value={props.eur.base}>{props.eur.base}</option>
+        <option value={props.usd.base}>{props.usd.base}</option>
+      </select>
     </div>
   )
 }
 
-export default currencyChoice;
+export default CurrencyChoice;
 
--- src/container/main.js
+++ src/container/main.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/header/header';
+import Currencies from "../components/currencies/currency-choice";
+import axios from 'axios'
+
+
 
 const Main = () => {
+  const [eur, setEur] = useState({});
+  const [chf, setChf] = useState({});
+  const [gbp, setGbp] = useState({});
+  const [usd, setUsd] = useState({});
+
+  useEffect(() => {
+    const eur = `https://api.exchangeratesapi.io/latest?base=EUR`;
+    const gbp = `https://api.exchangeratesapi.io/latest?base=GBP`;
+    const usd = `https://api.exchangeratesapi.io/latest?base=USD`;
+    const chf = `https://api.exchangeratesapi.io/latest?base=CHF`;
+
+    const fetchData = async (url, func) => {
+        await axios.get(url)
+          .then((res) => {
+            const new_rate_obj =  {
+              base: res.data.base,
+              gbp: res.data.rates.GBP,
+              chf: res.data.rates.CHF,
+              usd: res.data.rates.USD,
+              eur: res.data.rates.EUR
+            };
+            Object.keys(new_rate_obj).forEach(key => new_rate_obj[key] === undefined ? delete new_rate_obj[key] : {});
+            func(new_rate_obj);
+          });
+    };
+    fetchData(eur, setEur);
+    fetchData(gbp, setGbp);
+    fetchData(usd, setUsd);
+    fetchData(chf, setChf);
+  }, []);
+
   return (
     <div>
       <Header 
         content = "Damon's Currency Converter"
+      />
+      <Currencies
+        eur = {eur}
+        gbp = {gbp}
+        chf = {chf}
+        usd = {usd}
       />
     </div>
   )
